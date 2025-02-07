8faff3bcdd1dd6bfc4c551d65fe5e470e71852d4
Basel Diab <114107867+Baseldiab@users.noreply.github.com>
2025-02-07 03:05:34 +0200

    update header of contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,10 @@
+// util
 import { generateMetadata } from "@/components/util/generate-metadata";
 
-// components home
-import HomeHeaderSection from "@/components/home/header-section";
-import BlogPostSection from "@/components/home/blog-post-section";
-import BlogsSection from "@/components/home/blogs-section/blogs-section";
+// assets
+import HeaderBg from "@/public/images/header-details.webp";
+
+// components contact
 import ContactForm from "@/components/contact-us/contact-form";
 
 export const metadata = generateMetadata({
@@ -13,11 +14,20 @@ export const metadata = generateMetadata({
 export default function ContactPage() {
   return (
     <main className="flex-1 p-0 bg-gray-50">
-      <header className="flex items-center justify-center md:mt-[100px] mt-[100px]">
-        <h1 className="text-3xl font-bold py-5">Contact Us</h1>
+      <header className="flex items-center justify-center relative w-full h-[220px]  max-h-[300px]">
+        <div
+          style={{ backgroundImage: `url(${HeaderBg.src})` }}
+          className="absolute inset-0 bg-cover bg-center bg-no-repeat w-full h-full object-cover"
+        />
+
+        <div className="absolute inset-0 bg-[#343A40] opacity-80" />
+
+        <h1 className="text-3xl font-bold py-5 text-theme-text-lightTitle z-30 md:mt-8 mt-4">
+          Contact Us
+        </h1>
       </header>
 
-      <section className=" container pb-10  !max-w-[860px]">
+      <section className=" container md:py-14 sm:py-8 py-6  !max-w-[860px]">
         <ContactForm />
       </section>
     </main>
